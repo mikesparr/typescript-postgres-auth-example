50fff276af743dba1cc94bf943f22505b78ef0df
Mike Sparr <msparr@pointsixsolutions.com>
2019-02-01 17:14:24 -0700

    Progress on #28 - get feature flags for given user

--- src/utils/flag.helper.ts
+++ src/utils/flag.helper.ts
@@ -2,6 +2,7 @@
  * Generate feature flag lists that apply to given users
  * and manage cached values
  */
+import jexl from "jexl";
 import cache from "../config/cache"; // Redis commands
 import logger from "../config/logger";
 import { getRepository, Repository } from "typeorm";
@@ -16,6 +17,11 @@ import { Segment } from "../services/segment/segment.entity";
 const FEATURE_FLAGS_KEY = "feature:flags";
 const USER_FLAGS_KEY = (userId: number | string) => `user:${userId}:flags`;
 
+/**
+ * Returns list of feature flags that apply to given user
+ * TODO: potentially return {key, goalIds: []} instead of just the key strings
+ * @param user
+ */
 const getFlagsForUser = async (user: User): Promise<string[]> => {
   const userFlags: string[] = [];
 
@@ -23,8 +29,18 @@ const getFlagsForUser = async (user: User): Promise<string[]> => {
   const flags: Flag[] = await flagRepository.find({ relations: ["goals", "segments"] });
 
   flags.forEach((flag: Flag) => {
-    // TODO: check if flag applies to user
-    userFlags.push(flag.key);
+    // check if flag applies to user
+    const addFlag: boolean = false; // TODO: change to let once editing
+
+    // loop through segments
+      // check if user.id in excluded; if so - skip record immediately
+      // check if user.id in included; if so - set addFlag = true
+      // if addFlag still false, loop through rules
+        // if all conditions are true - set addFlag = true
+
+    if (addFlag) {
+      userFlags.push(flag.key);
+    }
   });
 
   return userFlags;
