4dccfb627f399f01adfb08dde9aeaad884e72cfa
Mike Sparr <msparr@pointsixsolutions.com>
2019-02-07 22:31:52 -0700

    Stubbing out integration test for events feature #41

--- src/services/event/__tests__/controller.test.ts
+++ src/services/event/__tests__/controller.test.ts
@@ -3,3 +3,11 @@ import controllers from "../../index";
 import { Application } from "express";
 import { getConnection, Connection } from "typeorm";
 import App from "../../../app";
+
+describe("events", () => {
+  describe("GET /events", () => {
+    it("returns list of events in descending order (recent)", () => {
+      expect(true).toBeTruthy();
+    });
+  });
+});
